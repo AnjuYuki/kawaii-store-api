c14ee6bdad9348a6b2e325c7a05161351c8d8fae
AnjuYuki <yuukianju1023@gmail.com>
2026-05-31 15:33:48 +0900

    feat: 新しいドキュメント構成とコンポーネントを追加し、依存関係を更新

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="min-h-screen py-10">
       <div className="mx-auto w-full max-w-5xl px-4">
         <h1 className="text-center text-4xl font-bold">Home</h1>
+        <Link href={'/docs'}>Document →</Link>
       </div>
     </div>
   );
--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,9 +1,16 @@
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
     ...components,
   } satisfies MDXComponents;
 }
